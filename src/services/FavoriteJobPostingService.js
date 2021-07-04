78a59781541b114c9009193f7d1f3ddc37bb123a
seyituludag0 <seyituludag0@gmail.com>
2021-07-05 01:50:27 +0300

    Refactoring

--- src/services/FavoriteJobPostingService.js
+++ src/services/FavoriteJobPostingService.js
@@ -9,18 +9,7 @@ export default class CityService {
     return axios.get(`http://localhost:8080/api/favoriteJobPostings/getByCandidateIdAndJobPostingId?candidateId=${candidateId}&jobPostingId=${jobPostingId}`)
   }
 
-  // addFavorites(jobPosting) {
-  //   return axios.post("http://localhost:8080/api/favoriteJobPostings/add", jobPosting);
-  // }
   addFavorites(candidateId, jobPostingId) {
     return axios.post(`http://localhost:8080/api/favoriteJobPostings/add?candidateId=${candidateId}&jobPostingId=${jobPostingId}`);
-  }
-
-  updateFavorites(values) {
-    return axios.post("http://localhost:8080/api/favoriteJobPostings/update", values);
-  }
-
-  delete(id){
-    return axios.delete("http://localhost:8080/api/favoriteJobPostings/delete?id=" + id)
   }
 }
